b7df211796711133f54912b80b0972ca2e69184e
aslannezgiii <aslanezgi98@gmail.com>
2021-05-25 01:05:53 +0300

    güncellendi

--- odev_3_2.py
+++ odev_3_2.py
@@ -1,25 +1,28 @@
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import matplotlib.ticker as ticker
 
 data = pd.read_csv('top50.csv')
 
 df = pd.DataFrame(data)
 #print(df)
 
-#veri tipinin floata çevrilmesi
+#veri tipinin float'a çevrilmesi
 df['Danceability'] = df['Danceability'].astype('float')
-
 #ilk 5 verinin gösterilmesi
 print(df.head())
-plt.figure(figsize=(12,6))
 
-x = np.arange(0.,50.,1.)
-x = list(x)
 
-plt.plot(x,df.Danceability,"-p", color="red")
+x = np.arange(0, 50,1)
 
+# Multiple Locator
+fig, ax = plt.subplots(1, 1)
+ax.plot(x,df.Danceability,"-p", color="red")
+ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+
+#plt.figure(figsize=(12,6))
+#plt.plot(x,df.Danceability,"-p", color="red") # çalışıyor -onar onar artıyor
 plt.title("Şarkıların Danceability Değerleri")
 plt.xlabel("Şarkı numarası")
 plt.ylabel("Danceability")
